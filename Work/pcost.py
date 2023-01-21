206fcbddc795dc1838bea33e381ea2e1a0beee32
Ro.Ma <roman.matviichuk@ukr.net>
2023-01-21 22:43:25 +0200

    [Ex. 1.27] Add the solution
    modified:   pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total = 0
+with open('Data/portfolio.csv') as file_obj:
+	headers = next(file_obj)
+	for line in file_obj:
+		items = line.split(',')
+		number = int(items[1])
+		price = float(items[2])
+		total += number * price
+		
+print("Total cost", total)
